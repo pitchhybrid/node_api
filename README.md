0f3acdd8c037fd0a4dd26cff565dccdc8063e50b
Messias <messias_junior@outlook.com>
2019-02-24 22:02:25 -0300

    project_initial

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# node_api
-CRUD INITIAL
+# BEGIN
+
+CRUD initial for study, is a Back-end created in node.js
+
+NO CREDENTIALS YET
+
+PLEASE EXCUSE-ME NOT USING ESLINT :)
+
+#INSTALL 
+
+<pre>
+yarn install && yarn dev
+</pre>
+
+#TOOLS
+
+
+<ul>
+    <li>DOOCKER WITH MONGODB</li>
+    <li>cors: 2.8.5</li>
+    <li>dotenv: 6.2.0</li>
+    <li>express: 4.16.4</li>
+    <li>mongoose: 5.4.15</li>
+    <li>mongoose-paginate-v2: 1.0.21</li>
+    <li>require-dir: 1.2.0</li>
+</ul>
+
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require('express'); //express-instance
+const mongoose = require('mongoose'); //mongodb ORM
+const cors = require('cors');   //CORS for EXECUTE in others servers
 const requireDir = require('require-dir');
+const dotenv = require('dotenv').load();
 
-const app = express();
+const env = process.env
+
+const app = express(); // Init app
 app.use(express.json());
 app.use(cors())
 //DB
 mongoose.connect(
-    'mongodb://127.0.0.1:27017/nodeapi',
+    env.DB_HOST,
     { useNewUrlParser:true });
 
 requireDir('./src/models');
 
 app.use('/api',require('./src/routes'));
 
-app.listen(3000);
+app.listen(env.API_PORT);
